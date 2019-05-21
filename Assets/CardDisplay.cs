b20e5a88fba4ce0c97e37345cd982503c2d903fb
David <davideduardo.zabalacuellar@elev.ga.ntig.se>
2019-05-21 15:21:43 +0200

    Scriptable Object

---
+++ Assets/CardDisplay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CardDisplay : MonoBehaviour
+{
+    public Card card;
+
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI descriptionText;
+    public Image artworkImage;
+    public TextMeshProUGUI manaText;
+    public TextMeshProUGUI attackText;
+    public TextMeshProUGUI healthText;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        nameText.text = card.name;
+        descriptionText.text = card.description;
+        artworkImage.sprite = card.artwork;
+        manaText.text = card.manaCost.ToString();
+        attackText.text = card.attack.ToString();
+        healthText.text = card.health.ToString();
+    }
+    
+}
